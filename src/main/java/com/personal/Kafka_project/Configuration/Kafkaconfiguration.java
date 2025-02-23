d8ed4a1cb841f0c74663256428ed9f0f8c36b245
ritesh0960 <riteshraushan0960@gmail.com>
2025-02-23 23:33:24 +0530

    adding programatically configured Kafka-producer and kafka-consumer

--- src/main/java/com/personal/Kafka_project/Configuration/Kafkaconfiguration.java
+++ src/main/java/com/personal/Kafka_project/Configuration/Kafkaconfiguration.java
@@ -1,15 +1,74 @@
 package com.personal.Kafka_project.Configuration;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class Kafkaconfiguration {
 
     @Bean
     public NewTopic newTopic(){
+
         return new NewTopic("studentTopic",4, (short) 1);
+    }
+
+    //Programtically configuring Producer
+
+    @Bean
+    public Map<String,Object> producerConfig(){
+        Map<String,Object> properties = new HashMap<>();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        properties.put(JsonDeserializer.TRUSTED_PACKAGES,"com.personal.Kafka_project.Entity");
+        return properties;
+    }
+
+    @Bean
+    public ProducerFactory<?,?> producerFactory(){
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    @Bean
+    public KafkaTemplate<?,?> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    //Configuring a Consumer Programatically
+
+    @Bean
+    public Map<String,Object> consumerConfig(){
+        Map<String,Object> properties = new HashMap<>();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG,"practiceGroup");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        properties.put(JsonDeserializer.TRUSTED_PACKAGES,"com.personal.Kafka_project.Entity");
+        return properties;
+    }
+
+    @Bean
+    public ConsumerFactory<String,Object> consumerFactory(){
+        return new DefaultKafkaConsumerFactory<>(consumerConfig());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String,Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
     }
 
 }
